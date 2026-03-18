d6df447be6fcdb57f445a984b43b0e099ae14bb6
khaiadvisory <hello@khaiadvisory.com>
2026-03-18 19:33:54 +0700

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1007,7 +1007,7 @@ function initWhyworkDiagramAnimation() {
 
   var reduceMotion = window.matchMedia && window.matchMedia('(prefers-reduced-motion: reduce)').matches;
   var mobileBreakpoint = 719;
-  var totalDuration = 3200;
+  var totalDuration = 3000;
   var repeatDelay = 650;
   var playTimer = null;
   var loopTimer = null;
